9a121c8b1b1ff83017bf848a0b9667b06f7ad37f
Krohn <krohnkg@gmail.com>
2024-10-07 14:26:12 +0200

    mass, mass center and f is calculated.

--- parameter_estimation/include/param_calc.h
+++ parameter_estimation/include/param_calc.h
@@ -24,25 +24,13 @@ public:
         Eigen::MatrixXd Matrix(72, 1);
         for (int i = 0; i < 24; i++)
         {
-            for(int j = 0; j < 3; j++)
-            {
-                Matrix(i, 0) = vector[i][j];
-            }
+            Matrix(i*3, 0) = vector[i][0];
+            Matrix(i*3+1, 0) = vector[i][1];
+            Matrix(i*3+2, 0) = vector[i][2];
         }
         return Matrix;
     }
-    static Eigen::MatrixXd conv_vector_to_72x3_matrix(std::vector<std::vector<float>> vector)
-    {
-        Eigen::MatrixXd Matrix(72, 3);
-        for (int i = 0; i < 72; i++)
-        {
-            for(int j = 0; j < 3; j++)
-            {
-                Matrix(i, j) = vector[i][j];
-            }
-        }
-        return Matrix;
-    }
+
     [[nodiscard]] Eigen::MatrixXd  define_A(std::vector<std::vector<float>> g)
     {
         for (int i = 0; i < 24; i++)
@@ -54,49 +42,70 @@ public:
             A(i*3  , 0) = 0;
             A(i*3 , 1) = gz;
             A(i*3 , 2) = -gy;
-            A(i*3+ 1 , 0) = gz;
+            A(i*3+ 1 , 0) = -gz;
             A(i*3+ 1 , 1) = 0;
-            A(i*3+ 1 , 2) = -gx;
-            A(i*3+2 , 0) = -gy;
-            A(i*3+2 , 1) = gx;
+            A(i*3+ 1 , 2) = gx;
+            A(i*3+2 , 0) = gy;
+            A(i*3+2 , 1) = -gx;
             A(i*3+2 , 2) = 0;
         }
         return A;
     }
-    void update_R(const Eigen::Matrix3f& R_matrix)
+    void update_R(std::vector<std::vector<float>> R1,
+                  std::vector<std::vector<float>> R2,
+                  std::vector<std::vector<float>> R3)
     {
-        R = R_matrix.cast<double>();
+        for (int i = 0; i < 24; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                R(i*3 ,j) = R1[i][j];
+                R(i*3+1 ,j) = R2[i][j];
+                R(i*3+2 ,j) = R3[i][j];
+            }
+        }
     }
 
 
     float m_calc()
     {
-        auto v1 =(G.transpose()*F);
-        auto v2 = (G.transpose()*G);
-        float m =v1.value()/v2.value();
+
+        Eigen::VectorXd Fv(Eigen::Map<Eigen::VectorXd>(F.data(), F.cols() * F.rows()));
+        Eigen::VectorXd Gv(Eigen::Map<Eigen::VectorXd>(G.data(), G.cols() * G.rows()));
+        float v1 = Gv.transpose().dot(Fv);
+        float v2 = Gv.transpose().dot(Gv);
+
+        float m =v1/v2;
         return m;
     }
-
+    Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
+    {
+        return A.completeOrthogonalDecomposition().pseudoInverse();
+    }
 
     [[nodiscard]] Eigen::MatrixXd  r_calc()
     {
-        auto r = 1 / m_calc() * (A.transpose()*A).inverse() * A.transpose() * T;
+        auto r = 1 / m_calc() * pseudo_inverse((A.transpose()*A)) * A.transpose() * T;
 
         return r;
     }
 
     [[nodiscard]] Eigen::MatrixXd  fg_calc()
     {
-        Eigen::MatrixXd f(2,1);
-        Eigen::Vector3d gi(3,1);
-        gi << 0, 0, -9.81;
-        Eigen::Vector3d r = r_calc();
+        Eigen::MatrixXd f(6,1);
+        Eigen::VectorXd f1(3,1);
+        Eigen::VectorXd f2(3,1);
+        Eigen::Vector3d  r = r_calc();
+        Eigen::Vector3d u1;
         Eigen::Vector3d u2;
-        Eigen::Vector3d U2;
-        u2 = R.transpose()*gi;
-        U2 = r.cross(u2);
-        f << m_calc()*R.transpose()*gi , m_calc()*U2;
 
+        u1 =  R.transpose()*G;
+        f1 = m_calc()*u1;
+
+        u2 = r.cross(u2);
+        f2 = m_calc()*u2;
+
+        f << f1(0,0),f1(1,0),f1(2,0), f2(0,0),f2(1,0),f2(2,0);
         return f;
     }
 
--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -4,21 +4,22 @@
 #include "../Data_parsing/Calibration.hpp"
 
 CalibrationFTS fileRead2("C:/Skulemappe/NTNU_2022-2025/5_Semester2023/AIS2202_Kubernetikk/Modul_2_State estimation/AIS2202_State_estimation_Gr3/Data/0-calibration_fts-accel.csv");
-Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
-{
-    return A.completeOrthogonalDecomposition().pseudoInverse();
-}
+
 
 int main()
 {
     std::vector<std::vector<float>> t = fileRead2.getTorqueVectorColumn();
     std::vector<std::vector<float>> g = fileRead2.getGVectorColumn();
     std::vector<std::vector<float>> f = fileRead2.getForceVectorColumn();
+    std::vector<std::vector<float>> r1 = fileRead2.getR1VectorColumn();
+    std::vector<std::vector<float>> r2 = fileRead2.getR2VectorColumn();
+    std::vector<std::vector<float>> r3 = fileRead2.getR3VectorColumn();
     parameter_calculation calc;
-    calc.update_Matrix(f,g,t);
-    Eigen::MatrixXd x = calc.define_A(g);
-    std::cout << x << std::endl;
+    calc.update_Matrix(f=f, g=g, t=t);
+    calc.update_R(r1,r2,r3);
 
-
+    std::cout <<"m: "<< calc.m_calc() << std::endl;
+    std::cout <<"r: "<< calc.r_calc() << std::endl;
+    std::cout <<"f: "<< calc.fg_calc() << std::endl;
     return 0;
 }
