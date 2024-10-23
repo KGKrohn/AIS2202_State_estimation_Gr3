a9a7804cda9a606f18a7fd196f6daf6c01e2b289
joelo <joeloe99@hotmail.com>
2024-10-23 19:41:02 +0200

    Added mapping functions to Vibration files too

--- Data_parsing/Vibration_Acc.hpp
+++ Data_parsing/Vibration_Acc.hpp
@@ -42,6 +42,10 @@ public:
         return {ax_[index], ay_[index], az_[index]};
     }
 
+    std::vector<float> getAccelerationMappedVector(const int &index) {
+        return {axmapped_[index], aymapped_[index], azmapped_[index]};
+    }
+
     std::vector<float> getAccVectorWithTime(const int &index) {
         return {t_[index], ax_[index], ay_[index], az_[index]};
     }
@@ -62,6 +66,27 @@ public:
         return column;
     }
 
+    void setVectorMapping(const float &sizeOfColumnWanted) {
+        float interval = float(t_.size()) / sizeOfColumnWanted;
+        for(int i = 0; i < sizeOfColumnWanted; i++) {
+            axmapped_.emplace_back(ax_[int(std::round(float(i) * interval))]);
+        }
+        for(int i = 0; i < sizeOfColumnWanted; i++) {
+            aymapped_.emplace_back(ay_[int(std::round(float(i) * interval))]);
+        }
+        for(int i = 0; i < sizeOfColumnWanted; i++) {
+            azmapped_.emplace_back(az_[int(std::round(float(i) * interval))]);
+        }
+    }
+
+    std::vector<std::vector<float>> getAccVectorMappingColumn() {
+        std::vector<std::vector<float>> column = {};
+        for (int i = 0; i < axmapped_.size(); i++) {
+            column.push_back(getAccelerationMappedVector(i));
+        }
+        return column;
+    }
+
     std::vector<float> getSingleTypeColumn_t_() {
         return t_;
     }
@@ -78,11 +103,26 @@ public:
         return az_;
     }
 
+    std::vector<float> getSingleTypeMappedColumn_ax() const {
+        return axmapped_;
+    }
+
+    std::vector<float> getSingleTypeMappedColumn_ay() const {
+        return aymapped_;
+    }
+
+    std::vector<float> getSingleTypeMappedColumn_az() const {
+        return azmapped_;
+    }
+
 private:
     std::vector<float> t_;
     std::vector<float> ax_;
     std::vector<float> ay_;
     std::vector<float> az_;
+    std::vector<float> axmapped_{};
+    std::vector<float> aymapped_{};
+    std::vector<float> azmapped_{};
     std::vector<std::string> variableNames_;
     std::vector<std::vector<float>> data_;
 };
